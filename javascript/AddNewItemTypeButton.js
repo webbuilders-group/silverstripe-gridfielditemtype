35d2637db4b253da35815afb603b2a7f5fb68ebb
IOTI <ryansm@outlook.com>
2013-07-26 10:48:00 -0300

    BUGFIX: added a entwine override event. When a user attempted to add an item type from a ModelAdmin and had already selected and added an item, the GET parameter from the previous item was being appended to the end of the URL meaning you couldn't change item types without refreshing.

--- javascript/AddNewItemTypeButton.js
+++ javascript/AddNewItemTypeButton.js
@@ -35,5 +35,18 @@
                 }
             }
         });
+        
+        /**
+		 * Overload the default GridField behaviour (open a new URL in the browser)
+		 * with the CMS-specific ajax loading. Unlike the entwine function found in
+		 * LeftAndMain.js, this ignores any current GET parameters which was 
+		 * causing issues when selecting different types to add.
+		 */
+		$('.cms #Form_EditForm .ss-gridfield').entwine({
+			showDetailView: function(url) {				
+				$('.cms-container').loadPanel(url);
+			}
+		});
+        
     });
 })(jQuery);
