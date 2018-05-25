d2d07eedef999d611bde4d389649d0f5067383e1
UndefinedOffset <echipman@webbuildersgroup.com>
2018-05-25 14:55:37 -0300

    Fixed potential crash when record is not an object

--- code/gridfield/ItemTypeDetailForm.php
+++ code/gridfield/ItemTypeDetailForm.php
@@ -48,20 +48,22 @@ class ItemTypeDetailForm_ItemRequest extends GridFieldDetailForm_ItemRequest {
     public function ItemEditForm() {
         $form=parent::ItemEditForm();
         
-        if(!$this->record->exists() && $this->request->getVar('ItemType')) {
-            if($addButton=$this->gridField->getConfig()->getComponentByType('AddNewItemTypeButton')) {
-                $values=$addButton->getRawDropdownValues();
-                
-                if(!array_key_exists($this->request->getVar('ItemType'), $values)) {
-                    user_error('The item type "'.htmlentities($request->getVar('ItemType')).'" is not one of the available item types', E_USER_ERROR);
+        if($this->record) {
+            if(!$this->record->exists() && $this->request->getVar('ItemType')) {
+                if($addButton=$this->gridField->getConfig()->getComponentByType('AddNewItemTypeButton')) {
+                    $values=$addButton->getRawDropdownValues();
+                    
+                    if(!array_key_exists($this->request->getVar('ItemType'), $values)) {
+                        user_error('The item type "'.htmlentities($request->getVar('ItemType')).'" is not one of the available item types', E_USER_ERROR);
+                    }
+                    
+                    $form->setFormAction(Controller::join_links($form->FormAction(), '?ItemType='.rawurlencode($this->request->getVar('ItemType'))));
+                }else {
+                    user_error('You must have the GridField Component "AddNewItemTypeButton" in your GridField config', E_USER_ERROR);
                 }
-                
-                $form->setFormAction(Controller::join_links($form->FormAction(), '?ItemType='.rawurlencode($this->request->getVar('ItemType'))));
-            }else {
-                user_error('You must have the GridField Component "AddNewItemTypeButton" in your GridField config', E_USER_ERROR);
+            }else if(!$this->record->exists()) {
+                user_error('No item type selected', E_USER_ERROR);
             }
-        }else if(!$this->record->exists()) {
-            user_error('No item type selected', E_USER_ERROR);
         }
         
         return $form;
