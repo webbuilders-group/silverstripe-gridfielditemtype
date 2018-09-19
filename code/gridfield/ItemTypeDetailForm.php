3de179986162458028c8e4a12693cab9dbc484ff
Robert LeCreux <robertlecreux@hotmail.com>
2018-09-19 15:23:12 -0300

    moved ItemTypeDetailDorm_ItemRequest to it's own file

--- code/gridfield/ItemTypeDetailForm.php
+++ code/gridfield/ItemTypeDetailForm.php
@@ -3,7 +3,6 @@
 namespace gridfielditemtype\code\gridfield;
 
 use SilverStripe\Forms\GridField\GridFieldDetailForm;
-use SilverStripe\Forms\GridField\GridFieldDetailForm_ItemRequest;
 
 class ItemTypeDetailForm extends GridFieldDetailForm {
     /**
@@ -42,37 +41,4 @@ class ItemTypeDetailForm extends GridFieldDetailForm {
     }
 }
 
-class ItemTypeDetailForm_ItemRequest extends GridFieldDetailForm_ItemRequest {
-    private static $allowed_actions=array(
-                                        'ItemEditForm'
-                                    );
-    
-    /**
-     * Generates the form with the item type in the action url
-     * @return Form
-     */
-    public function ItemEditForm() {
-        $form=parent::ItemEditForm();
-        
-        if($this->record) {
-            if(!$this->record->exists() && $this->request->getVar('ItemType')) {
-                if($addButton=$this->gridField->getConfig()->getComponentByType('AddNewItemTypeButton')) {
-                    $values=$addButton->getRawDropdownValues();
-                    
-                    if(!array_key_exists($this->request->getVar('ItemType'), $values)) {
-                        user_error('The item type "'.htmlentities($request->getVar('ItemType')).'" is not one of the available item types', E_USER_ERROR);
-                    }
-                    
-                    $form->setFormAction(Controller::join_links($form->FormAction(), '?ItemType='.rawurlencode($this->request->getVar('ItemType'))));
-                }else {
-                    user_error('You must have the GridField Component "AddNewItemTypeButton" in your GridField config', E_USER_ERROR);
-                }
-            }else if(!$this->record->exists()) {
-                user_error('No item type selected', E_USER_ERROR);
-            }
-        }
-        
-        return $form;
-    }
-}
 ?>
